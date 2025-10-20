04fe75528c25233229984ce14bab8fc033b89e75
NicolasEstebanCuervo <rojascuervo942@gmail.com>
2025-10-20 16:38:18 -0500

    Commit

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,39 +1,49 @@
-import { useState } from 'react'
-import { evaluate } from 'mathjs'
+import { useState } from "react";
+import { evaluate } from "mathjs";
 
-export const operations = ['+', '-', '*', '/']
-const equalSign = '='
+export const operations = ["+", "-", "*", "/"];
+const equalSign = "=";
 
-export const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]]
+export const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
 
 export const Calculator = () => {
-  const [value, setValue] = useState('')
+    const [value, setValue] = useState("");
 
-  const createHandleClick = (op) => () => setValue(value.concat(op))
+    const createHandleClick = (op) => () => setValue(value.concat(op));
 
-  return (
-    <div>
-      <h1>Calculator</h1>
-      <input value={value} readOnly />
-      <div role='grid'>
-        {rows.map((row, idx) => (
-          <div key={idx} role='row'>
-            {row.map((number) => (
-              <button onClick={createHandleClick(number)} key={number}>
-                {number}
-              </button>
-            ))}
-          </div>
-        ))}
+    return (
+        <div>
+            <h1>Calculator</h1>
 
-        {operations.map((operation) => (
-          <button onClick={createHandleClick(operation)} key={operation}>
-            {operation}
-          </button>
-        ))}
+            <h1>Calculator</h1>
+            <input value={value} readOnly />
+            <div role="grid">
+                {rows.map((row, idx) => (
+                    <div key={idx} role="row">
+                        {row.map((number) => (
+                            <button
+                                onClick={createHandleClick(number)}
+                                key={number}
+                            >
+                                {number}
+                            </button>
+                        ))}
+                    </div>
+                ))}
 
-        <button onClick={() => setValue(evaluate(value))}>{equalSign}</button>
-      </div>
-    </div>
-  )
-}
+                {operations.map((operation) => (
+                    <button
+                        onClick={createHandleClick(operation)}
+                        key={operation}
+                    >
+                        {operation}
+                    </button>
+                ))}
+
+                <button onClick={() => setValue(evaluate(value))}>
+                    {equalSign}
+                </button>
+            </div>
+        </div>
+    );
+};
